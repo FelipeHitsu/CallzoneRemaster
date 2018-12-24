e5e6667e0de3e6fdfaae7940313b3bcd3e3d292c
Sidnei <sidneicarrarojunior@gmail.com>
2018-12-24 15:43:02 -0200

    ricochet falta a explosão

--- CallZone/Assets/Scripts/Bullet/RicochetBullet.cs
+++ CallZone/Assets/Scripts/Bullet/RicochetBullet.cs
@@ -10,9 +10,13 @@ public class RicochetBullet : MonoBehaviour
     //Velocidade de movimento do projetil
     public float _speed;
     //Vida(quantidade de vezes que vai bater)
-    private int _ricoLife = 3;
+    private int _ricoLife = 5;
     //Vai ativar a explosão
     private bool _onExplosion = false;
+    //Particula quando bate nas coisas
+    public GameObject _ricoParticle;
+    //Explosão que vai dar dano
+    public GameObject _ricoExplosion;
 
     public LayerMask collisionMask;
 
@@ -25,6 +29,8 @@ public class RicochetBullet : MonoBehaviour
 
     void Update()
     {
+        
+
         //Movimentação do projétil
         bulletRb.MovePosition(transform.position + transform.right * _speed * Time.deltaTime);
 
@@ -38,6 +44,22 @@ public class RicochetBullet : MonoBehaviour
         //Verificando a colisão do raycast
         if (hit.collider != null)
         {
+            //Instanciando a particula quando bate
+            GameObject tempPart = Instantiate(_ricoParticle, transform.position, Quaternion.identity, transform);
+
+            //Destruindo a particula 
+            Destroy(tempPart, 1.5f);
+
+            //Diminuindo a vida para saber quando ela vai explodir
+            _ricoLife -= 1;
+            Debug.Log("Vai explodir em...:" + _ricoLife);
+
+            //Se chegar a 0, explode!
+            if (_ricoLife <= 0)
+            {
+                _onExplosion = true;
+            }   
+           
             //Vetor de reflexão
             Vector2 reflection = Vector2.Reflect(ray.direction, hit.normal);
             //Reflect retorna o valor contrário do vetor definido como normal
@@ -47,6 +69,17 @@ public class RicochetBullet : MonoBehaviour
 
             transform.eulerAngles = new Vector3(0, 0, rot);    
         }
+
+        if(_onExplosion)
+        {
+            //Instanciando a particula
+            GameObject tempExplo = Instantiate(_ricoExplosion, transform.position, Quaternion.identity, transform);
+            //DEstruindo a particula
+            Destroy(tempExplo, 2.0f);
+
+            _onExplosion = false;
+            _ricoLife = 5;
+        }
     }
 
 
@@ -54,4 +87,6 @@ public class RicochetBullet : MonoBehaviour
     {
         _playerNumber = player;
     }
+
+    
 }
